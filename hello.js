8390c4cf583965d2cc4708b28e631d16c60a2f1a
DavidVegas1991 <davidvegas1991@gmail.com>
2021-02-02 12:10:14 +0100

    solucionado el problema del popup de google

--- hello.js
+++ hello.js
@@ -3,9 +3,18 @@ const {Builder, By, Key, until}  = require('selenium-webdriver');
 
 (async function hello(){
     let driver = await new Builder().forBrowser('firefox').build();
-    await driver.get('https://www.google.com/');
-    await driver.findElement(By.id('gsr')).click();
-    await driver.findElement(By.name('q')).sendKeys("cheese!\n",Key.RETURN);
 
+    //vamos a la url
+    await driver.get('https://www.google.com/');
+
+    //nos movemos al frame 0
+    await driver.switchTo().frame(0);
+    //clickamos en el boton
+    await driver.wait(until.elementLocated(By.id('introAgreeButton'))).click();
+    //regresamos al frame principal
+    await driver.switchTo().defaultContent();
+    //hacemos buscqueda
+    await driver.findElement(By.name('q')).sendKeys("cheese!\n",Key.RETURN);
+    
 })();
 
